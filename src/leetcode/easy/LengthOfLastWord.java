2058448b1bd2361b466d2170117dc3c236203731
julian <julianhuang2010@protonmail.com>
2019-04-19 08:53:28 +0800

    https://leetcode.com/problems/plus-one/

---
+++ src/leetcode/easy/LengthOfLastWord.java
@@ -0,0 +1,18 @@
+package leetcode.easy;
+
+public class LengthOfLastWord {
+
+  public static int lengthOfLastWord(String s) {
+    if(s.isEmpty()){
+      return 0;
+    }
+     s =s.trim();
+     s = s.substring(s.lastIndexOf(" ")+1,s.length());
+    return s.length();
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(LengthOfLastWord.lengthOfLastWord(" a"));
+  }
+  
+}
