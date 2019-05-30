30cae2f5dfb7886bafec9c31c3dc91962030b1e9
julian <julianhuang2010@protonmail.com>
2019-05-30 13:57:17 +0800

    https://leetcode.com/problems/kth-largest-element-in-a-stream

---
+++ src/leetcode/easy/array/KthLargest.java
@@ -0,0 +1,45 @@
+package leetcode.easy.array;
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+
+    final PriorityQueue<Integer> q;
+    final int k;
+
+    public KthLargest(int k, int[] a) {
+        this.k = k;
+        q = new PriorityQueue<>(k);
+        for (int n : a) {
+            q.add(n);
+        }
+    }
+
+    public int add(int n) {
+
+        if (q.size() < k)
+            q.offer(n);
+         else if (q.peek() < n) {
+            q.poll();
+            q.offer(n);
+        }
+
+        return q.peek();
+    }
+
+
+    public static void main(String[] args) {
+        int k = 3;
+        int[] arr = {4, 5, 8, 2};
+        KthLargest kthLargest = new KthLargest(k, arr);
+
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+        System.out.println(kthLargest.add(9));
+        System.out.println(kthLargest.add(4));
+
+    }
+
+
+}
--- src/leetcode/easy/stack/MyQueue.java
+++ src/leetcode/easy/stack/MyQueue.java
@@ -44,15 +44,17 @@ public class MyQueue {
 
     public static void main(String[] args) {
         MyQueue obj = new MyQueue();
-//        obj.push(1);
-//        obj.push(2);
-//        int param_2 = obj.peek();
-//        obj.push(3);
-//        int param_3 = obj.peek();
+        obj.push(1);
+        obj.push(2);
+        int param_2 = obj.peek();
+        obj.push(3);
+        int param_3 = obj.pop();
+        int param_5 = obj.pop();
+        int param_6 = obj.pop();
         boolean param_4 = obj.empty();
 
-//        System.out.println(param_2);
-//        System.out.println(param_3);
+        System.out.println(param_2);
+        System.out.println(param_3);
         System.out.println(param_4);
 
     }
