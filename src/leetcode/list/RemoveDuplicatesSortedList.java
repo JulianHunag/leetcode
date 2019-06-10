e62277c6866fa4836aaba888a51670c743d4a63d
julian <julianhuang2010@protonmail.com>
2019-06-10 13:59:53 +0800

    https://leetcode.com/problems/remove-duplicates-from-sorted-list/

--- src/leetcode/list/RemoveDuplicatesSortedList.java
+++ src/leetcode/list/RemoveDuplicatesSortedList.java
@@ -1,8 +1,24 @@
 package leetcode.list;
 
+import leetcode.pojo.ListNode;
+
+/**
+ * 83 Remove Duplicates from Sorted List
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
 public class RemoveDuplicatesSortedList {
 
+    public ListNode deleteDuplicates(ListNode head) {
 
-
-
+        ListNode curr = head;
+        if (null == curr) return null;
+        while (null != curr.next) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
 }
