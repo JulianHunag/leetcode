a472deddc5d97019bf9f422ce8505f97446b9ccd
julian <julianhuang2010@protonmail.com>
2019-06-21 18:16:31 +0800

    https://leetcode.com/problems/array-partition-i/

---
+++ src/leetcode/array/ArrayPartitionI.java
@@ -0,0 +1,21 @@
+package leetcode.array;
+
+
+import java.util.Arrays;
+
+/**
+ * 561. Array Partition I
+ * https://leetcode.com/problems/array-partition-i
+ */
+public class ArrayPartitionI {
+
+    public int arrayPairSum(int[] nums) {
+        int res = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length;i+=2){
+            res += nums[i];
+        }
+        return res;
+    }
+
+}
